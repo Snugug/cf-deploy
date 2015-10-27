ec9c6cecdde6802c5a18e777049b644b0b182130
Sam Richard <snugug@gmail.com>
2015-10-27 14:47:46 -0700

    :art::new: Clean up and add full CFBlob path

--- index.js
+++ index.js
@@ -5,36 +5,24 @@ var Promise = require('bluebird'),
     Spinner = require('cli-spinner').Spinner,
     spinner = new Spinner('%s'),
     manifest = require('./lib/manifest'),
-    config = require('./lib/config'),
-    cf = require('./lib/cf');
+    create = require('./lib/create'),
+    blob = require('./lib/cfblob');
 
 module.exports = function (options) {
   spinner.start();
 
-  config(options).then(function (config) {
-    cf = cf(config);
+  return blob(options).then(function (cfBlob) {
+    spinner.stop();
+    console.log(cfBlob.app);
+    // return create(cfBlob).then(function (app) {
+    //   spinner.stop();
+    //   console.log(app);
+    // });
+    // return manifest(cfBlob).then(function (settings) {
+    //   spinner.stop();
+    //   // console.log(settings);
 
-    return cf.cloudFoundry.getInfo().then(function (result) {
-      return cf.cloudFoundry.login(result.authorization_endpoint,
-        config.username,
-        config.password);
-    }).then(function (auth) {
-      return cf.apps.getApps(auth.token_type, auth.access_token).then(function (result) {
-        var guid;
-
-        result.resources.forEach(function (item) {
-          if (item.entity.name === config.app) {
-            guid = item.metadata.guid;
-          }
-        });
-
-        return manifest(auth, guid, cf).then(function (settings) {
-          spinner.stop();
-          console.log(settings);
-          return settings;
-        });
-      });
-    })
+    // });
   }).catch(function (error) {
     spinner.stop(true);
     console.error('Error: ' + error);
