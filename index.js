ed2e9499934e9bf5f402a16971ae58f56000a705
Sam Richard <snugug@gmail.com>
2015-10-27 16:07:59 -0700

    :new: Zip creation for file upload

--- index.js
+++ index.js
@@ -6,27 +6,33 @@ var Promise = require('bluebird'),
     spinner = new Spinner('%s'),
     manifest = require('./lib/manifest'),
     create = require('./lib/create'),
-    blob = require('./lib/cfblob');
+    blob = require('./lib/cfblob'),
+    zip = require('./lib/zip');
+
 
 module.exports = function (options) {
-  spinner.start();
-
-  return blob(options).then(function (cfBlob) {
-    spinner.stop();
-    console.log(cfBlob.app);
-    // return create(cfBlob).then(function (app) {
-    //   spinner.stop();
-    //   console.log(app);
-    // });
-    // return manifest(cfBlob).then(function (settings) {
-    //   spinner.stop();
-    //   // console.log(settings);
-
-    // });
-  }).catch(function (error) {
-    spinner.stop(true);
-    console.error('Error: ' + error);
-    process.exit(1);
+  return zip().then(function (zip) {
+    console.log(zip);
   });
+
+  // spinner.start();
+
+  // return blob(options).then(function (cfBlob) {
+  //   spinner.stop();
+  //   console.log(cfBlob.app);
+  //   // return create(cfBlob).then(function (app) {
+  //   //   spinner.stop();
+  //   //   console.log(app);
+  //   // });
+  //   // return manifest(cfBlob).then(function (settings) {
+  //   //   spinner.stop();
+  //   //   // console.log(settings);
+
+  //   // });
+  // }).catch(function (error) {
+  //   spinner.stop(true);
+  //   console.error('Error: ' + error);
+  //   process.exit(1);
+  // });
 }
 
